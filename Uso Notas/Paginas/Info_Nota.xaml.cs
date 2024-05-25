31eafe55e794387e165432299ee36198b9bb9ef0
Hernan Mateo Jurado Moran <hernan.jurado@udla.edu.ec>
2024-05-24 19:42:28 -0500

    Info_Nota

--- Uso Notas/Paginas/Info_Nota.xaml.cs
+++ Uso Notas/Paginas/Info_Nota.xaml.cs
@@ -1,9 +1,57 @@
 namespace Uso_Notas.Paginas;
+[QueryProperty(nameof(ItemId), nameof(ItemId))]
+
 
 public partial class Info_Nota : ContentPage
 {
-	public Info_Nota()
+    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
+    public Info_Nota()
 	{
 		InitializeComponent();
-	}
+        string appDataPath = FileSystem.AppDataDirectory;
+        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+
+        LoadNote(Path.Combine(appDataPath, randomFileName));
+    }
+
+    private async void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Modelo.Nota note)
+            File.WriteAllText(note.Filename, TextEditor.Text);
+
+        await Shell.Current.GoToAsync("..");
+    }
+
+    private async void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Modelo.Nota note)
+        {
+            // Delete the file.
+            if (File.Exists(note.Filename))
+                File.Delete(note.Filename);
+        }
+
+        await Shell.Current.GoToAsync("..");
+    }
+
+    private void LoadNote(string fileName)
+    {
+        Modelo.Nota noteModel = new Modelo.Nota();
+        noteModel.Filename = fileName;
+
+        if (File.Exists(fileName))
+        {
+            noteModel.Date = File.GetCreationTime(fileName);
+            noteModel.Text = File.ReadAllText(fileName);
+        }
+
+        BindingContext = noteModel;
+    }
+
+    public string ItemId
+    {
+        set { LoadNote(value); }
+    }
+
 }
