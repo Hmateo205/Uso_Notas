f283a62b216d4de16d047e3851cc44c10a6c1ce5
Hernan Mateo Jurado Moran <hernan.jurado@udla.edu.ec>
2024-05-24 20:28:59 -0500

    Documentos C#

--- Uso Notas/Modelo/Info.cs
+++ Uso Notas/Modelo/Info.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 namespace Uso_Notas.Modelo
 {
     internal class Info
-    {
-    }
+    { 
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://aka.ms/maui";
+        public string Message => "This app is written in XAML and C# with .NET MAUI.";
+}
 }
--- Uso Notas/Modelo/Nota.cs
+++ Uso Notas/Modelo/Nota.cs
@@ -8,5 +8,8 @@ namespace Uso_Notas.Modelo
 {
     internal class Nota
     {
+        public string Filename { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
     }
 }
